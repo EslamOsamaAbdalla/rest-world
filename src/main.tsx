a12757c99afbd69136b4ad8e09a1904526a65a56
= <=>
2024-05-14 15:21:11 +0200

    fixed routing path /:countryName

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "//rest-world",
+        path: "/rest-world",
         element: <Home />,
       },
       {
